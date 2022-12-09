007027add43541af14826414fe837c6776ff87df
Your Name <you@example.com>
2022-12-09 16:04:23 -0700

    PROYECTO

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
     @livewireStyles
 </head>
 
-<body class="hold-transition sidebar-mini layout-fixed">
+<body class="dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed sidebar-closed sidebar-collapse" style="height: auto;">
     <div class="wrapper">
 
         <!-- Preloader -->
